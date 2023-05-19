144214c47ec3a057c0f237003a441f5b101af602
argojuni <juniargoprasaja05@gmail.com>
2023-05-19 11:23:04 +0700

    playerControls

---
+++ PingPong/Assets/Script/PlayerControls.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public KeyCode moveUp;
+    public KeyCode moveDown;
+    public float speed = 10;
+
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();    
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(moveUp))
+        {
+            rb.velocity = new Vector2(rb.velocity.y, speed);
+        }
+        else if (Input.GetKey(moveDown)) {
+            rb.velocity = new Vector2(rb.velocity.y, speed*-1);
+        }
+        else
+        {
+            rb.velocity = new Vector2(rb.velocity.y, 0);
+        }
+    }
+}
